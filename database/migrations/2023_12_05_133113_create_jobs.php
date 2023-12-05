1ba6098eb1ce3d797b0ff31dc8c64dad8898019f
tinghaolai <analth355@gmail.com>
2023-12-05 22:09:40 +0800

    Migration to create jobs

---
+++ database/migrations/2023_12_05_133113_create_jobs.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('source');
+            $table->string('company');
+            $table->string('job_name');
+            $table->string('url');
+            $table->unsignedInteger('min_monthly_salary')->nullable();
+            $table->unsignedInteger('max_monthly_salary')->nullable();
+            $table->unsignedInteger('min_annual_salary')->nullable();
+            $table->unsignedInteger('max_annual_salary')->nullable();
+            $table->float('rating')->nullable();
+            $table->boolean('illegal')->default(false);
+            $table->text('comment')->nullable();
+            $table->timestamp('last_check_time')->nullable();
+            $table->timestamp('last_seen')->nullable();
+            $table->timestamp('delivery_time')->nullable();
+            $table->boolean('starred')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
